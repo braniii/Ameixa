0bf4f235426442df622cf020b693463be965edb8
xphnx <rober@localhost.localdomain>
2018-08-17 17:41:00 +0200

    new icons batch and process_script minor fix

--- process_wipsvg-xml.sh
+++ process_wipsvg-xml.sh
@@ -23,27 +23,33 @@ N=$(basename $SVG .svg)
 done
 
 # "xml" crea los correspondientes values/iconpack.xml y xml/drawable.xml
-SVGDIR="icons" #esto puede obviarse? que hace code.xml?
-ICPACK_PRE='<item>'
+SVGDIR="icons/"
+EXPORT="app/src/main/res"
+ICPACK_PRE='        <item>'
 ICPACK_SUF='</item>\n'
-DRAWABLE_PRE='<item drawable="'
+DRAWABLE_PRE='    <item drawable="'
 DRAWABLE_SUF='" />\n'
-CODE_PRE='R.drawable.nodpi_'
+CODE_PRE='            R.drawable.nodpi_'
 CODE_SUF=',\n'
 
-printf '<?xml version="1.0" encoding="utf-8"?>\n<resources>\n <string-array name="icon_pack" translatable="false">\n' > app/src/main/res/values/iconpack.xml
-printf '<?xml version="1.0" encoding="utf-8"?>\n<resources>\n <version>1</version>\n' > app/src/main/res/xml/drawable.xml
-#printf 'private Integer[] mImages = {\n'> code.xml
+printf '<?xml version="1.0" encoding="utf-8"?>\n<resources>\n    <string-array name="icon_pack" translatable="false">\n' > iconpack.xml
+printf '<?xml version="1.0" encoding="utf-8"?>\n<resources>\n    <version>1</version>\n' > drawable.xml
+printf '    private Integer[] mImages = {\n' > code.xml
 
-for DIR in $(find ${SVGDIR} -name "*.svg")
+for DIR in $(find ${SVGDIR} -name "*.svg" | sort)
 do
   FILE=${DIR##*/}
   NAME=${FILE%.*}
-  printf "${ICPACK_PRE}${NAME}${ICPACK_SUF}" >> app/src/main/res/values/iconpack.xml
-  printf "${DRAWABLE_PRE}${NAME}${DRAWABLE_SUF}" >> app/src/main/res/xml/drawable.xml 
- #printf "${CODE_PRE}${NAME}${CODE_SUF}" >> code.xml
+  printf "${ICPACK_PRE}${NAME}${ICPACK_SUF}" >> iconpack.xml
+  printf "${DRAWABLE_PRE}${NAME}${DRAWABLE_SUF}" >> drawable.xml 
+  printf "${CODE_PRE}${NAME}${CODE_SUF}" >> code.xml
 done
 
-printf '</string-array>\n</resources>\n' >> app/src/main/res/values/iconpack.xml
-printf '</resources>\n' >> app/src/main/res/xml/drawable.xml
-#printf '};' >> code.xml
+printf '    </string-array>\n</resources>\n' >> iconpack.xml
+printf '</resources>\n' >> drawable.xml
+printf '    };' >> code.xml
+
+rm -rf code.xml
+
+mv -f iconpack.xml ${EXPORT}/values/
+mv -f drawable.xml ${EXPORT}/xml/
